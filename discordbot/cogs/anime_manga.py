3dcd6ec30307a82f360f131489bb98149c74f982
Lunecron <Lunecron@live.de>
2024-02-29 03:19:16 +0100

    anime_manga.py : Added timeout and timer , Update colors, Changed anime paginator defer
    Added a timeout for the paginator and buttonview
    Changed color of mangadex embeds
    Changed for anime search defer to True because False is broken

--- discordbot/cogs/anime_manga.py
+++ discordbot/cogs/anime_manga.py
@@ -3,11 +3,13 @@ import requests
 import json
 import discord
 import os
-from discord.ext import commands
+from discord.ext import commands,tasks
 from discord.commands import Option
 import json
 from bs4 import BeautifulSoup
 import time
+import asyncio
+
 
 #To convert html to markdown
 import markdownify
@@ -17,6 +19,9 @@ bot_main_directory = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 temp_images_directory = os.path.join(bot_main_directory, 'temp_images')
 
 temp_filename= "temp.jpg"
+
+# Dictionary to store scheduled deletions
+scheduled_deletions = {}
 
 #Sorting for mangadex
 order = {
@@ -63,6 +68,7 @@ def setup(bot) -> None:
 
 async def anilist(ctx,name : str):
     #Using Anilist API for Anime
+    deletion_time = 60 # Time till message gets deleted
     series_type = "Anime"
     anime_info = search_anime_info(name)
 
@@ -79,15 +85,21 @@ async def anilist(ctx,name : str):
 
             series_type += "/"+ format_correct
 
+        if anime_info['english_title'] == None or anime_info['english_title'] == '':
+                anime_info['english_title'] = anime_info['romaji_title']
+                anime_info['romaji_title'] = None
 
 
         embed = discord.Embed(title=anime_info['english_title'], description=description_markdown, color=0x7289da, url=f"https://anilist.co/anime/{anime_info['id']}")
         embed.add_field(name="Typ", value=str(series_type), inline=False)
-        embed.add_field(name="Romaji Title", value=anime_info['romaji_title'], inline=False)
+        if anime_info['romaji_title'] != None or anime_info['romaji_title'] !='':
+            embed.add_field(name="Romaji Title", value=anime_info['romaji_title'], inline=False)
         embed.set_thumbnail(url=anime_info['cover_image_url'])
         name_and_search_type = [name,"anime"]
         button_view = ButtonView(ctx,name_and_search_type)
-        await ctx.followup.send(embed = embed, view=button_view) 
+        message = await ctx.followup.send(embed = embed, view=button_view) 
+        scheduled_deletions[message.id] = asyncio.create_task(delete_after_delay(ctx,message,deletion_time,button_view))
+
         # Enable the button after 1 second
         await button_view.enable_after_delay()
     else:
@@ -149,6 +161,7 @@ def search_anime_info(title):
 async def mangadex(ctx, name):
     #Using mangadex api for Webtoon and Manga
     base_url = "https://api.mangadex.org"
+    deletion_time = 60 # Time till message gets deleted
     manga_params = {"title": name, **final_order_query}
     mangadex_response = requests.get(f"{base_url}/manga", params=manga_params)
     has_thumbnail:  bool = False
@@ -214,7 +227,7 @@ async def mangadex(ctx, name):
             if has_thumbnail:
                 file_and_dir = os.path.join(temp_images_directory,temp_filename)
                 file = discord.File(file_and_dir)  
-                embed = discord.Embed(title=title, description=description, color=0x7289da,url=f'https://mangadex.org/manga/{id}')
+                embed = discord.Embed(title=title, description=description, color=0xffa500,url=f'https://mangadex.org/manga/{id}')
                 embed.add_field(name="Typ", value=str(series_type), inline=False)
                 if alt_title != '':
                     embed.add_field(name="Alt Title", value=alt_title, inline=False)
@@ -222,17 +235,21 @@ async def mangadex(ctx, name):
                 name_and_search_type = [name,"manga"]
                 file_entry = {'filename' : temp_filename, 'file_and_dir':file_and_dir}
                 button_view = ButtonView(ctx,name_and_search_type,file_entry)
-                await ctx.followup.send(file=file, embed=embed , view=button_view)
+                message = await ctx.followup.send(file=file, embed=embed , view=button_view)
+                scheduled_deletions[message.id] = asyncio.create_task(delete_after_delay(ctx,message,deletion_time,button_view))
+
                 file.close()
                 # Enable the button after 1 second
                 await button_view.enable_after_delay()
             else:
-                embed = discord.Embed(title=title, description=description, color=0x7289da,url=f'https://mangadex.org/manga/{id}')
+                embed = discord.Embed(title=title, description=description, color=0xffa500,url=f'https://mangadex.org/manga/{id}')
                 embed.add_field(name="Typ", value=str(series_type), inline=False)
                 embed.add_field(name="Alt Title", value=alt_title, inline=False)
                 name_and_search_type = [name,"manga"]
                 button_view = ButtonView(ctx,name_and_search_type)
-                await ctx.followup.send(embed = embed, view=button_view) 
+                message = await ctx.followup.send(embed = embed, view=button_view) 
+                scheduled_deletions[message.id] = asyncio.create_task(delete_after_delay(ctx,message,deletion_time,button_view))
+
                 # Enable the button after 1 second
                 await button_view.enable_after_delay()
             
@@ -302,9 +319,12 @@ class ButtonView(discord.ui.View):
         self.ctx = ctx
         self.filename = []
         self.file_path = []
+        self.timer_button = self.get_timer_button()
         self.get_filenames_and_files(file_entry)
         self.name = name_and_search_type[0]
         self.search_type = name_and_search_type[1]
+        
+
 
     async def on_timeout(self):
         if self is not None:
@@ -320,9 +340,15 @@ class ButtonView(discord.ui.View):
         time.sleep(1)
         # Enable the button
         for child in self.children:
-            if isinstance(child, discord.ui.Button):
+            if isinstance(child, discord.ui.Button) and child.custom_id != "button_timer":
                 child.disabled = False
         await self.message.edit(view=self)
+
+    def get_timer_button(self):
+        for child in self.children:
+            if isinstance(child, discord.ui.Button) and child.custom_id == "button_timer":
+                return child
+        return None
     
     def delete_files(self):
         #Prevent deleting no_cover.jpg
@@ -333,6 +359,21 @@ class ButtonView(discord.ui.View):
         if file_entry != []:
             self.filename.append(file_entry['filename'])
             self.file_path.append(file_entry['file_and_dir'])
+
+    #Update timer label, deletion is handeld seperatly
+    async def update_timer_countdown(self, duration):
+        for remaining_time in range(duration, 0, -1):
+            print(remaining_time)
+            # Update the button label with the remaining time
+            if remaining_time <=60:
+                self.timer_button.label =f"{remaining_time} sec"
+            else:
+                self.timer_button.label =f"{remaining_time/60} min"
+
+            await self.message.edit(view=self)  
+            # Wait for 1 second
+            await asyncio.sleep(1)
+    
         
 
     @discord.ui.button(label="Correct", style=discord.ButtonStyle.green, emoji="✅", custom_id="button_correct", disabled=True)
@@ -341,6 +382,10 @@ class ButtonView(discord.ui.View):
         if interaction.message.embeds[0]:
             await interaction.response.send_message(embed=interaction.message.embeds[0])
         await original_message.delete()
+        if original_message.id in scheduled_deletions:
+            # Cancel the scheduled deletion task
+            scheduled_deletions[original_message.id].cancel()
+            del scheduled_deletions[original_message.id]
         if self.file_path != []:
             self.delete_files()
         self.stop()
@@ -348,7 +393,6 @@ class ButtonView(discord.ui.View):
     @discord.ui.button(label="Search", style=discord.ButtonStyle.primary, emoji="🔍", custom_id="button_search", disabled=True)
     async def button_search(self,button,interaction):
         original_message = interaction.message
-
         #If manga
         if self.search_type.lower() == "manga":
             await interaction.response.defer()
@@ -358,40 +402,60 @@ class ButtonView(discord.ui.View):
 
         #Is anime
         elif self.search_type.lower() == "anime":
+            await interaction.response.defer()
             entries = await search_entries_anilist(self.name)
             shorten_embeds,complete_embeds = await shorten_and_complete_embed_searchresults_anilist(entries)
-            await paginate_search(interaction,self.ctx,shorten_embeds,complete_embeds,defer=False)
+            await paginate_search(interaction,self.ctx,shorten_embeds,complete_embeds,defer=True)
         else:
             await interaction.response.send_message("No corresponding type found.", ephemeral= True ,delete_after=20)
         await original_message.delete()
+        if original_message.id in scheduled_deletions:
+            # Cancel the scheduled deletion task
+            scheduled_deletions[original_message.id].cancel()
+            del scheduled_deletions[original_message.id]
 
     @discord.ui.button(label="Dismiss", style=discord.ButtonStyle.red, emoji="✖️", custom_id="button_dismiss", disabled=True)
     async def button_dismiss(self,button,interaction):
         original_message = interaction.message
         await interaction.response.send_message("Search deleted." ,ephemeral= True ,delete_after=20)
+        if original_message.id in scheduled_deletions:
+            # Cancel the scheduled deletion task
+            scheduled_deletions[original_message.id].cancel()
+            del scheduled_deletions[original_message.id]
         await original_message.delete()
         if self.file_path != []:
             self.delete_files()
         self.stop()
+
+    @discord.ui.button(label="", style=discord.ButtonStyle.gray, emoji="⏰", custom_id="button_timer", disabled=True)
+    async def button_timer(self,button,interaction):
+        await interaction.response.defer()
+
+    
 
 
 class PaginatorView(discord.ui.View):
     def __init__(self, ctx, shorten_data, complete_data, file_entries = []):
         super().__init__(timeout=None)
         self.ctx = ctx
+        self.delete_timer = 5
         self.data = shorten_data
         self.complete_data = complete_data
         self.filenames = []
         self.file_paths = []
         self.get_filenames_and_files(file_entries)
         self.current_page = 0
+        self.timer_button = self.get_timer_button()
         self.prev_button = self.get_prev_button()
+        self.current_page_button = self.get_current_page_button()
         self.next_button = self.get_next_button()
         self.check_for_enabled_buttons()
+        self.current_page_button.label = f'{self.current_page+1}/{len(self.data)}'
 
     async def on_timeout(self):
         if self is not None:
             await self.message.edit(view=None)
+            
 
     def check_for_enabled_buttons(self):
         #Disable Buttons that can not be used
@@ -430,6 +494,11 @@ class PaginatorView(discord.ui.View):
             await interaction.response.send_message(embed=self.complete_data[self.current_page], view=None)
             await self.message.delete()
 
+        if self.message.id in scheduled_deletions:
+            # Cancel the scheduled deletion task
+            scheduled_deletions[self.message.id].cancel()
+            del scheduled_deletions[self.message.id]
+
         if self.file_paths != []:
             self.delete_files()
         self.stop()
@@ -439,6 +508,10 @@ class PaginatorView(discord.ui.View):
         self.current_page = max(0, self.current_page - 1)
         await self.update_buttons()
         await self.update_message(interaction)
+
+    @discord.ui.button(label="1/X",style=discord.ButtonStyle.gray, custom_id="button_current_page_paginator", disabled=True)
+    async def current_page_button(self, button: discord.ui.Button, interaction: discord.Interaction):
+        await interaction.response.defer()
         
 
     @discord.ui.button(label="Next",style=discord.ButtonStyle.primary, custom_id="button_next_paginator", disabled=True)
@@ -452,10 +525,19 @@ class PaginatorView(discord.ui.View):
     async def dismiss_button(self, button: discord.ui.Button, interaction: discord.Interaction):
         await interaction.response.send_message("Paginator dismissed.", ephemeral=True, delete_after=20)
         await self.message.delete()
+
+        if self.message.id in scheduled_deletions:
+            # Cancel the scheduled deletion task
+            scheduled_deletions[self.message.id].cancel()
+            del scheduled_deletions[self.message.id]
+
         if self.file_paths != []:
             self.delete_files()
         self.stop()
-        
+    
+    @discord.ui.button(label="", style=discord.ButtonStyle.gray, emoji="⏰", custom_id="button_timer", disabled=True)
+    async def button_timer(self,button,interaction):
+        await interaction.response.defer()
 
     async def update_message(self,interaction):
         await interaction.response.defer()
@@ -471,6 +553,7 @@ class PaginatorView(discord.ui.View):
 
     async def update_buttons(self):
         #Disable Buttons that can not be used
+        self.current_page_button.label = f'{self.current_page+1}/{len(self.data)}'
         if len(self.data)-1 == 0: 
             self.prev_button.disabled = True
             self.next_button.disabled = True
@@ -491,37 +574,83 @@ class PaginatorView(discord.ui.View):
             if isinstance(child, discord.ui.Button) and child.custom_id == "button_prev_paginator":
                 return child
         return None
+    
+    def get_current_page_button(self):
+        for child in self.children:
+            if isinstance(child, discord.ui.Button) and child.custom_id == "button_current_page_paginator":
+                return child
+        return None
 
     def get_next_button(self):
         for child in self.children:
             if isinstance(child, discord.ui.Button) and child.custom_id == "button_next_paginator":
                 return child
         return None
+    
+    def get_timer_button(self):
+        for child in self.children:
+            if isinstance(child, discord.ui.Button) and child.custom_id == "button_timer":
+                return child
+        return None
 
+#Function to delete messages after a delay (delete on timeout), default deletion time 180 = 3min
+async def delete_after_delay(interaction_or_ctx,message, deletion_time = 180 , view = None):
+    for remaining_time in range(deletion_time, 0, -1):
+        print(remaining_time)
+        if view:
+            # Update the button label with the remaining time
+            if remaining_time <=60:
+                view.timer_button.label =f"{remaining_time} sec"
+            else:
+                minutes, seconds= divmod(remaining_time, 60)
+                if seconds == 0:
+                    seconds = '00'
+                view.timer_button.label =f"{minutes}:{seconds} min"
+
+            await view.message.edit(view=view)  
+            # Wait for 1 second
+        await asyncio.sleep(1)
+
+
+    if message.id in scheduled_deletions:
+        try:
+            await interaction_or_ctx.followup.send(content= "You did not select anything. Selection dismissed.",ephemeral=True)
+            await message.delete()
+        except discord.NotFound:
+            pass  # Message already deleted
 
 #Paginate the shorten_embeds
 async def paginate_search(interaction,ctx,shorten_embeds,complete_embeds,file_entries = [], defer = False):
+    deletion_time= 180
     if file_entries != []:
         if defer:
             paginator_view = PaginatorView(ctx, shorten_embeds,complete_embeds,file_entries)
             file = discord.File(file_entries[0]['file_and_dir'])
             message = await interaction.followup.send(file = file,embed=shorten_embeds[0], view=paginator_view)
+            scheduled_deletions[message.id] = asyncio.create_task(delete_after_delay(interaction,message,deletion_time,paginator_view))
             paginator_view.message = message
             file.close()
         else:
+            # Not working
             paginator_view = PaginatorView(ctx, shorten_embeds,complete_embeds,file_entries)
             file = discord.File(file_entries[0]['file_and_dir'])
-            message = await interaction.response.send_message(file = file,embed=shorten_embeds[0], view=paginator_view)
+            await interaction.response.send_message(file = file,embed=shorten_embeds[0], view=paginator_view)
+            message = await interaction.original_response()
+            scheduled_deletions[message.id] = asyncio.create_task(delete_after_delay(interaction,message,deletion_time,paginator_view))
             paginator_view.message = message
             file.close()
     else:
         if defer:
             paginator_view = PaginatorView(ctx, shorten_embeds,complete_embeds)
             message = await interaction.followup.send(embed=shorten_embeds[0], view=paginator_view)
+            scheduled_deletions[message.id] = asyncio.create_task(delete_after_delay(interaction,message,deletion_time,paginator_view))
             paginator_view.message = message
         else:
+            # Not working
             paginator_view = PaginatorView(ctx, shorten_embeds,complete_embeds)
-            message = await interaction.response.send_message(embed=shorten_embeds[0], view=paginator_view)
+            await interaction.response.send_message(embed=shorten_embeds[0], view=paginator_view)
+            message = await interaction.original_response()
+            scheduled_deletions[message.id] = asyncio.create_task(delete_after_delay(interaction,message,deletion_time,paginator_view))
             paginator_view.message = message
     
 
@@ -727,7 +856,7 @@ async def shorten_and_complete_embed_searchresults_mangadex(data):
 
             filename = entry['filename']
             id = entry['id']
-            shorten_embed = discord.Embed(title=entry['english_title'], color=0x7289da,url=f'https://mangadex.org/manga/{id}')
+            shorten_embed = discord.Embed(title=entry['english_title'], color=0xffa500,url=f'https://mangadex.org/manga/{id}')
             shorten_embed.add_field(name="Typ", value=str(series_type), inline=False)
             if entry['romaji_title'] != '':
                 shorten_embed.add_field(name="Alt Title", value=entry['romaji_title'], inline=False)
@@ -735,7 +864,7 @@ async def shorten_and_complete_embed_searchresults_mangadex(data):
             shorten_embed.set_thumbnail(url=f'attachment://{filename}')
             shorten_embeded_data.append(shorten_embed)
 
-            complete_embed = discord.Embed(title=entry['english_title'], description=entry['description'], color=0x7289da,url=f'https://mangadex.org/manga/{id}')
+            complete_embed = discord.Embed(title=entry['english_title'], description=entry['description'], color=0xffa500,url=f'https://mangadex.org/manga/{id}')
             complete_embed.add_field(name="Typ", value=str(series_type), inline=False)
             if entry['romaji_title'] != '':
                 complete_embed.add_field(name="Alt Title", value=entry['romaji_title'], inline=False)
